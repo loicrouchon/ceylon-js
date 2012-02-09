7bb923f3c3153df1713e999ab6c6be1c31ab2b73
Enrique Zamudio <alterbass@gmail.com>
2012-02-09 14:16:53 -0600

    Put Integer, Float and some numeric-related interfaces in a separate file

--- runtime/ceylon/language/0.1/ceylon.language.js
+++ runtime/ceylon/language/0.1/ceylon.language.js
@@ -131,31 +131,11 @@ function Summable(wat) {
 }
 initType(Summable, 'ceylon.language.Summable');
 exports.Summable=Summable;
-function Number(wat) {
-    return wat;
-}
-initType(Number, 'ceylon.language.Number', Equality);
-exports.Number=Number;
 function Invertable(wat) {
     return wat;
 }
 initType(Invertable, 'ceylon.language.Invertable');
 exports.Invertable=Invertable;
-function Numeric(wat) {
-    return wat;
-}
-initType(Numeric, 'ceylon.language.Numeric', Number, Comparable, Summable, Invertable);
-exports.Numeric=Numeric;
-function Ordinal(wat) {
-    return wat;
-}
-initType(Ordinal, 'ceylon.language.Ordinal', Equality);
-exports.Ordinal=Ordinal;
-function Integral(wat) {
-    return wat;
-}
-initType(Integral, 'ceylon.language.Integral', Numeric, Ordinal);
-exports.Integral=Integral;
 function Ranged(wat) {
     return wat;
 }
@@ -222,92 +202,19 @@ $Exception.prototype.getString = function() {
     return exports.String('Exception "' + this.getMessage().value + '"');
 }
 
-function Integer(value) {
-    var that = new Integer.$$;
-    that.value = value;
-    return that;
+var _subm=require('ceylon/language/0.1/numbers');
+for (var $ in _subm) {
+    exports[$]=_subm[$];
 }
-initType(Integer, 'ceylon.language.Integer', Object$, Castable, Integral, Numeric);
-inheritProto(Integer, Object$, '$Object$');
-exports.Integer=Integer;
-var $Integer = Integer.$$;
-$Integer.prototype.getString = function() { return exports.String(this.value.toString()) }
-$Integer.prototype.plus = function(other) { return Integer(this.value+other.value) }
-$Integer.prototype.minus = function(other) { return Integer(this.value-other.value) }
-$Integer.prototype.times = function(other) { return Integer(this.value*other.value) }
-$Integer.prototype.divided = function(other) {
-    var exact = this.value/other.value;
-    return Integer((exact<0) ? Math.ceil(exact) : Math.floor(exact));
-}
-$Integer.prototype.remainder = function(other) { return Integer(this.value%other.value) }
-$Integer.prototype.power = function(other) {
-    var exact = Math.pow(this.value, other.value);
-    return Integer((exact<0) ? Math.ceil(exact) : Math.floor(exact));
-}
-$Integer.prototype.getNegativeValue = function() { return Integer(-this.value) }
-$Integer.prototype.getPositiveValue = function() { return this }
-$Integer.prototype.equals = function(other) { return Boolean$(other && other.value===this.value) }
-$Integer.prototype.compare = function(other) {
-    return this.value===other.value ? equal
-                                    : (this.value<other.value ? smaller:larger);
-}
-$Integer.prototype.getFloat = function() { return Float(this.value) }
-$Integer.prototype.getInteger = function() { return this }
-$Integer.prototype.getCharacter = function() { return exports.Character(this.value); }
-$Integer.prototype.getSuccessor = function() { return Integer(this.value+1) }
-$Integer.prototype.getPredecessor = function() { return Integer(this.value-1) }
-$Integer.prototype.getUnit = function() { return Boolean$(this.value === 1) }
-$Integer.prototype.getZero = function() { return Boolean$(this.value === 0) }
-$Integer.prototype.getFractionalPart = function() { return Integer(0); }
-$Integer.prototype.getWholePart = function() { return this; }
-$Integer.prototype.getSign = function() { return this.value > 0 ? Integer(1) : this.value < 0 ? Integer(-1) : Integer(0); }
-$Integer.prototype.getHash = function() { return this; }
+var Integer=_subm.Integer;
+var Float=_subm.Float;
+
 
 function $parseInteger(s) { return Integer(parseInt(s.value)); }
 function $parseFloat(s) { return Float(parseFloat(s.value)); }
 exports.parseInteger=$parseInteger;
 exports.parseFloat=$parseFloat;
 
-function Float(value) {
-    var that = new Float.$$;
-    that.value = value;
-    return that;
-}
-initType(Float, 'ceylon.language.Float', Object$, Castable, Numeric);
-inheritProto(Float, Object$, '$Object$');
-exports.Float=Float;
-var $Float = Float.$$;
-$Float.prototype.getString = function() { return exports.String(this.value.toString()) }
-$Float.prototype.plus = function(other) { return Float(this.value+other.value) }
-$Float.prototype.minus = function(other) { return Float(this.value-other.value) }
-$Float.prototype.times = function(other) { return Float(this.value*other.value) }
-$Float.prototype.divided = function(other) { return Float(this.value/other.value) }
-$Float.prototype.power = function(other) { return Float(Math.pow(this.value, other.value)) }
-$Float.prototype.getNegativeValue = function() { return Float(-this.value) }
-$Float.prototype.getPositiveValue = function() { return this }
-$Float.prototype.equals = function(other) { return Boolean$(other && other.value===this.value) }
-$Float.prototype.compare = function(other) {
-    if (other === null || other === undefined) { return larger; }
-    return this.value===other.value ? equal
-                                    : (this.value<other.value ? smaller:larger);
-}
-$Float.prototype.getFloat = function() { return this }
-$Float.prototype.getInteger = function() { return Integer(parseInt(this.value.toFixed())); }
-$Float.prototype.getWholePart = function() {
-    var _p = this.value.toPrecision();
-    var dot = _p.indexOf('.');
-    return dot >= 0 ? Float(parseFloat(_p.slice(0, dot))) : this;
-}
-$Float.prototype.getFractionalPart = function() {
-    var _p = this.value.toPrecision();
-    var dot = _p.indexOf('.');
-    return dot >= 0 ? Float(parseFloat(_p.slice(dot))) : Float(0.0);
-}
-$Float.prototype.getSign = function() { return this.value > 0 ? Integer(1) : this.value < 0 ? Integer(-1) : Integer(0); }
-$Float.prototype.getHash = function() { return exports.String(this.value.toPrecision()).getHash(); }
-$Float.prototype.getUndefined = function() { return isNaN(this.value) ? $true : $false; }
-$Float.prototype.getFinite = function() { return this.value!==Infinity && this.value!==-Infinity && !isNaN(this.value) ? $true : $false; }
-$Float.prototype.getInfinite = function() { return this.value===Infinity || this.value===-Infinity ? $true : $false; }
 
 function getInfinity() { return Float(Infinity); }
 //function getNegativeInfinity() { return Float(-Infinity); }
